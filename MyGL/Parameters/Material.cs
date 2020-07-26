19034655653d4ddaab98455f2cf16d05ad702d2e
Олег Есиков <esikov.oleg@mail.ru>
2020-07-26 11:49:21 +0300

    Переработка иерархии материалов

--- MyGL/Parameters/Material.cs
+++ MyGL/Parameters/Material.cs
@@ -10,26 +10,18 @@ using OpenTK.Input;
 
 namespace MyGL
 {
-    public class Material
+    public class Material : Parameters
     {
-        public Vector3 Ambient { get; private set; }
-        public Vector3 Diffuse { get; private set; }
-        public Vector3 Specular { get; private set; }
-        public float Shininess { get; private set; }
+        public float Shininess { get; set; }
 
         public Material()
         {
-            Ambient = new Vector3(1.0f, 1.0f, 1.0f);
-            Diffuse = new Vector3(1.0f, 1.0f, 1.0f);
-            Specular = new Vector3(1.0f, 1.0f, 1.0f);
-            Shininess = 32.0f;
+            Shininess = 32f;
         }
 
-        public Material(Vector3 ambient, Vector3 diffuse, Vector3 specular, float shininess)
+        public Material(Vector3 ambient, Vector3 diffuse, Vector3 specular, float shininess = 32f)
+            : base(ambient, diffuse, specular)
         {
-            Ambient = ambient;
-            Diffuse = diffuse;
-            Specular = specular;
             Shininess = shininess;
         }
 
@@ -37,5 +29,30 @@ namespace MyGL
         {
             return new Material(ambient, diffuse, specular, shininess);
         }
+
+        // Материалы
+        public static Material Brass => new Material(
+            new Vector3(0.329412f, 0.223529f, 0.027451f),
+            new Vector3(0.780392f, 0.568627f, 0.113725f), 
+            new Vector3(0.992157f, 0.941176f, 0.807843f)
+        );
+
+        public static Material Bronze => new Material(
+            new Vector3(0.2125f, 0.1275f, 0.054f),
+            new Vector3(0.714f, 0.4284f, 0.18144f),
+            new Vector3(0.393548f, 0.271906f, 0.166721f)
+        );
+
+        public static Material Jade => new Material(
+            new Vector3(0.135f, 0.2225f, 0.1575f),
+            new Vector3(0.54f, 0.89f, 0.63f),
+            new Vector3(0.316228f, 0.316228f, 0.316228f)
+        );
+
+        public static Material YellowPlastic => new Material(
+            new Vector3(0.0f, 0.0f, 0.0f),
+            new Vector3(0.5f, 0.5f, 0.0f),
+            new Vector3(0.60f, 0.60f, 0.50f)
+        );
     }
 }
